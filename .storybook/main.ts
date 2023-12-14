ea48a70e00431148cb1a88b88a84a21d41e2c81e
jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2023-12-14 10:01:18 +0100

    add storybook-vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,23 @@
-import type { StorybookConfig } from "@storybook/angular";
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+import type { StorybookViteConfig } from '@storybook/builder-vite';
+
+
+const config: StorybookViteConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
   },
 };
 export default config;
